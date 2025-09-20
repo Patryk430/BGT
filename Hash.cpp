f9e4923c5ec26e5bb721d01bc14da1187086e420
Patryk430 <patryk.survilo@mif.stud.vu.lt>
2025-09-20 16:50:47 +0300

    Vartotojo sasaja ir isvedimas i faila

--- Hash.cpp
+++ Hash.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <sstream>
 #include <random>
@@ -11,6 +12,25 @@ std::string Random_String (int length)
     std::uniform_int_distribution<> distr(65, 122); // A-65, z-122
 
     std::string string = "";
+
+    for (int i = 0; i<length; i++)
+    {
+        char c = distr(gen);
+        string+=c;
+    }
+
+    return string;
+}
+
+std::string Random_String ()
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distr(65, 122); // A-65, z-122
+    std::uniform_int_distribution<> l_distr(500, 1500); // A-65, z-122
+
+    std::string string = "";
+    int length = l_distr(gen);
 
     for (int i = 0; i<length; i++)
     {
@@ -84,58 +104,141 @@ void Test_2_strings (int test_count,int string_length)
 {
     int max_hash_similarity = 0;
     int min_hash_similarity = 64;
+    int sum_hash_similarity = 0;
+
+    std::ofstream O ("Same_Length_String_Test_Results.txt");
 
     for (int i=0; i<test_count; i++)
     {
         std::string string_1 = Random_String(string_length);
         std::string string_2 = Random_String(string_length);
 
-        std::cout << string_1 << " " << string_2 << " Panašumas: " << String_Similarity(string_1, string_2) << "/" << string_length << std::endl;
+        O << string_1 << " " << string_2 << " Panašumas: " << String_Similarity(string_1, string_2) << "/" << string_length << std::endl;
         
         std::string hash_1 = Hash(string_1);
         std::string hash_2 = Hash(string_2);
 
         int similarity = String_Similarity(hash_1, hash_2);
 
-        std::cout << hash_1 << " " << hash_2 << " Panašumas: " << similarity << "/64\n";
+        O << hash_1 << " " << hash_2 << " Panašumas: " << similarity << "/64\n";
+
+        sum_hash_similarity += similarity;
 
         if (max_hash_similarity < similarity) max_hash_similarity = similarity;
         if (min_hash_similarity > similarity) min_hash_similarity = similarity;
     }
 
-    std::cout << "Didžiausias Hash'ų panašumas: " << max_hash_similarity << "/64\n" << "Mažiausias Hash'ų panašumas: " << min_hash_similarity << "/64\n";   
+    O << "\nDidžiausias Hash'ų panašumas: " << max_hash_similarity << "/64\n" << "Mažiausias Hash'ų panašumas: " << min_hash_similarity << "/64\n" << "Vidutinis Hash'ų panašumas: " << (double)sum_hash_similarity/test_count;
+
+    O.close();   
+}
+
+void Test_2_different_strings (int test_count)
+{
+    int max_hash_similarity = 0;
+    int min_hash_similarity = 64;
+    int sum_hash_similarity = 0;
+
+    std::ofstream O ("Different_String_Test_Results.txt");
+
+    for (int i=0; i<test_count; i++)
+    {
+        std::string string_1 = Random_String();
+        std::string string_2 = Random_String();
+
+        O << string_1 << " " << string_2 << std::endl;
+        
+        std::string hash_1 = Hash(string_1);
+        std::string hash_2 = Hash(string_2);
+
+        int similarity = String_Similarity(hash_1, hash_2);
+
+        O << hash_1 << " " << hash_2 << " Panašumas: " << similarity << "/64\n";
+
+        sum_hash_similarity += similarity;
+
+        if (max_hash_similarity < similarity) max_hash_similarity = similarity;
+        if (min_hash_similarity > similarity) min_hash_similarity = similarity;
+    }
+
+    O << "\nDidžiausias Hash'ų panašumas: " << max_hash_similarity << "/64\n" << "Mažiausias Hash'ų panašumas: " << min_hash_similarity << "/64\n" << "Vidutinis Hash'ų panašumas: " << (double)sum_hash_similarity/test_count;
+
+    O.close();
 }
 
 void Test_2_simillar_strings (int test_count,int string_length)
 {
     int max_hash_similarity = 0;
     int min_hash_similarity = 64;
+    int sum_hash_similarity = 0;
+
+    std::ofstream O ("Simillar_String_Test_Results.txt");
 
     for (int i=0; i<test_count; i++)
     {
         std::string string_1 = Random_String(string_length);
         std::string string_2 = Similar_String(string_1);
 
-        std::cout << string_1 << " " << string_2 << " Panašumas: " << String_Similarity(string_1, string_2) << "/" << string_length << std::endl;
+        O << string_1 << " " << string_2 << " Panašumas: " << String_Similarity(string_1, string_2) << "/" << string_length << std::endl;
         
         std::string hash_1 = Hash(string_1);
         std::string hash_2 = Hash(string_2);
 
         int similarity = String_Similarity(hash_1, hash_2);
 
-        std::cout << hash_1 << " " << hash_2 << " Panašumas: " << similarity << "/64\n";
+        O << hash_1 << " " << hash_2 << " Panašumas: " << similarity << "/64\n";
+
+        sum_hash_similarity += similarity;
 
         if (max_hash_similarity < similarity) max_hash_similarity = similarity;
         if (min_hash_similarity > similarity) min_hash_similarity = similarity;
     }
 
-    std::cout << "Didžiausias Hash'ų panašumas: " << max_hash_similarity << "/64\n" << "Mažiausias Hash'ų panašumas: " << min_hash_similarity << "/64\n";   
+    O << "\nDidžiausias Hash'ų panašumas: " << max_hash_similarity << "/64\n" << "Mažiausias Hash'ų panašumas: " << min_hash_similarity << "/64\n" << "Vidutinis Hash'ų panašumas: " << (double)sum_hash_similarity/test_count;
+
+    O.close();
 }
+
 
 int main()
 {  
-    Test_2_simillar_strings(1000, 20);
+    int choice = 0;
+    
+    std::cout << "SVEIKI\n" 
+    << "--------------------------------------------------------------------------------------------------\n" 
+    << "1 - Patikrinti 1 000 000 to pačio ilgio skirtingų string'ų porų\n"
+    << "2 - Patikrinti 1 000 000 to pačio ilgio panašių (1 simbolio skirtumas) string'ų porų\n"
+    << "3 - Patikrinti 1 000 000 skirtingo ilgio string'ų porų\n"
+    << "4 - Testas su failų 'input.txt'\n"
+    << "5 - Patikrinti deterministiškumą\n";
 
+    std::cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+        Test_2_strings (1000000, 1000);
+        break;
+    
+    case 2:
+        Test_2_simillar_strings (1000000, 1000);
+        break;
+
+    case 3:
+        Test_2_different_strings (1000000);
+        break;
+
+    case 4:
+        //Test_file()
+        break;
+
+    case 5:
+        //Test_deter()
+        break;
+
+    default:
+        break;
+    }
     return 0;
 }
 
